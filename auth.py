1c87fb9538dbee8e1672eed559bdc700a97b7e32
Hari Sankar <harisankar4001@gmail.com>
2024-05-05 10:28:34 +0200

    Forget password with password reset apis

--- auth.py
+++ auth.py
@@ -4,8 +4,9 @@ import jwt
 from datetime import datetime, timedelta
 from flask import Blueprint, request, jsonify
 from mongo_manager import MongoManager
-from utils import password_reset
+from utils import password_reset, validate_token
 from dotenv import load_dotenv
+from decorators import login_required
 
 load_dotenv()
 logger = logging.Logger(__name__)
@@ -15,29 +16,72 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 mongo = MongoManager(AUTH_DB)
-collection = os.getenv('AUTH_COLLECTION')
+customer_collection = os.getenv('AUTH_COLLECTION')
 
 
 @auth_blueprint.route("/api/v1/login", methods=['POST'])
-def select_operation():
-    body = request.json
-    if 'email' in body:
-        email = body['email']
-        customer = mongo.find_one(collection, {'email': email})
-        if customer:
-            if 'password' in customer:
-                if 'password' not in body:
-                    return jsonify({'message': 'Missing email or password'}), 400
+def login():
+    try:
+        body = request.json
+        domain = request.host_url
+        if 'email' in body:
+            email = body['email']
+            customer = mongo.find_one(customer_collection, {'email': email})
+            if customer:
+                if 'password' in customer:
+                    if 'password' not in body:
+                        return jsonify({'message': 'Missing email or password'}), 400
+                    else:
+                        if customer['password'] == body['password']:
+                            token = jwt.encode({'customer_id': customer['customer_id'], 'customer_email': customer['email'],
+                                                'exp': datetime.utcnow() + timedelta(days=30)},
+                                               key=SECRET_KEY, algorithm="HS256")
+                            return jsonify({'token': token}), 200
                 else:
-                    if customer['password'] == body['password']:
-                        token = jwt.encode({'customer_id': customer['customer_id'],
-                                            'exp': datetime.utcnow() + timedelta(days=30)},
-                                           key=SECRET_KEY, algorithm="HS256")
-                        return jsonify({'token': token}), 200
+                    password_reset(customer, domain)
+                    return jsonify({'message': f'Link to reset the password have been sent to {email}'}), 400
             else:
-                password_reset(email)
-                return jsonify({'message': f'Link to reset the password have been sent to {email}'}), 400
+                return jsonify({'message': 'User not found'}), 404
         else:
-            return jsonify({'message': 'User not found'}), 404
-    else:
-        return jsonify({'message': 'Missing email or password'}), 400
+            return jsonify({'message': 'Missing email or password'}), 400
+    except Exception as E:
+        logger.exception(str(E))
+        return 500
+
+
+@auth_blueprint.route('/api/v1/forgot-password', methods=['POST'])
+def forgot_password():
+    try:
+        domain = request.host_url
+        body = request.json
+        if 'email' in body:
+            email = body['email']
+            customer = mongo.find_one(customer_collection, {'email': email})
+            if customer:
+                reset = password_reset(customer, domain)
+            return jsonify({'message': f'Link to reset the password have been sent to {email}'}), 400
+        return jsonify({'message': 'Missing email'}), 400
+    except Exception as E:
+        logger.exception(str(E))
+        return 500
+
+
+@auth_blueprint.route('/api/v1/reset-password', methods=['POST'])
+def reset_password():
+    try:
+        token = request.args.get('token')
+        new_password = request.json.get('new_password')
+
+        if token:
+            validate = validate_token(token)
+            if validate:
+                email = validate['email']
+                mongo.update_one(customer_collection, {'email': email}, {'password': new_password})
+                return jsonify({'message': 'Password reset successful'})
+            else:
+                return jsonify({'error': 'Token has expired'}), 400
+        else:
+            return jsonify({'error': 'Invalid token'}), 400
+    except Exception as E:
+        logger.exception(str(E))
+        return 500
