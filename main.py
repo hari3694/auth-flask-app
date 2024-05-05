3a12727a2c348d5680477ae55b6c4bf153d5f1ea
Hari Sankar <harisankar4001@gmail.com>
2024-05-05 10:30:19 +0200

    auth blueprints registered

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
-
+from auth import auth_blueprint
 
 app = Flask(__name__)
 
+app.register_blueprint(auth_blueprint)
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
